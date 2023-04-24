3c67b7a532803868ac7a3935cb86cbf5d7cd5ee7
rweesner <rweesner@smatechnologies.com>
2023-04-24 11:34:01 -0500

    First Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,54 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'SMA Technologies Help',
+  tagline: 'Template - Product Name',
+  url: 'https://help.smatechnologies.com',
+  baseUrl: '/training/opcon-mft-training/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'smatechnologies',
+  projectName: 'opcon-mft-training',
+  themeConfig: {
+    navbar: {
+      title: 'Help',
+      logo: {
+        alt: 'SMA Technologies Help Logo',
+        src: 'img/logo.svg',
+        href: 'https://help.smatechnologies.com',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} SMA Technologies.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          breadcrumbs: false,
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/smatechnologies/opcon-mft-training/blob/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-7XYMFXX81Y',
+          anonymizeIP: false,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'), 
+      {
+      }
+    ],
+  ],
+};
